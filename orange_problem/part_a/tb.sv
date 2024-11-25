f0733d5c1b2af6384013c8674d775153db796b69
Manasvi M T <145837882+svisvi413@users.noreply.github.com>
2024-11-25 19:22:15 +0530

    testbench added

---
+++ orange_problem/part_a/tb.sv
@@ -0,0 +1,23 @@
+module tb;
+    reg a, b, c, d;
+    wire y;
+    comb_func dut(a, b, c, d, y);
+    initial begin
+        a = 1'b0; b = 1'b0; c = 1'b0; d = 1'b0;
+        #10 a = 1'b0; b = 1'b0; c = 1'b0; d = 1'b1;
+        #10 a = 1'b0; b = 1'b0; c = 1'b1; d = 1'b0;
+        #10 a = 1'b0; b = 1'b0; c = 1'b1; d = 1'b1;
+        #10 a = 1'b0; b = 1'b1; c = 1'b0; d = 1'b0;
+        #10 a = 1'b0; b = 1'b1; c = 1'b0; d = 1'b1;
+        #10 a = 1'b0; b = 1'b1; c = 1'b1; d = 1'b0;
+        #10 a = 1'b0; b = 1'b1; c = 1'b1; d = 1'b1;
+        #10 a = 1'b1; b = 1'b0; c = 1'b0; d = 1'b0;
+        #10 a = 1'b1; b = 1'b0; c = 1'b0; d = 1'b1;
+        #10 a = 1'b1; b = 1'b0; c = 1'b1; d = 1'b0;
+        #10 a = 1'b1; b = 1'b0; c = 1'b1; d = 1'b1;
+        #10 a = 1'b1; b = 1'b1; c = 1'b0; d = 1'b0;
+        #10 a = 1'b1; b = 1'b1; c = 1'b0; d = 1'b1;
+        #10 a = 1'b1; b = 1'b1; c = 1'b1; d = 1'b0;
+        #10 a = 1'b1; b = 1'b1; c = 1'b1; d = 1'b1;
+    end
+endmodule
